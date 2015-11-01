f90c9ad428f420bbffe4556c5c6288f2720e50a8
Thomas Case <webmaster@francyscollection.com>
2015-11-01 09:50:10 -0800

    Added files to repository

---
+++ recaptcha.js
@@ -0,0 +1,83 @@
+
+module.exports = function ReCaptchaModule(pb) {
+
+    //pb dependencies
+    var util = pb.util;
+    
+    /**
+     * ReCaptcha - Constructor
+     * @class ReCaptcha
+     * @constructor
+     */
+    function ReCaptcha(){}
+    
+    /**
+     * The ID for the ReCaptcha provider
+     * @private
+     * @static
+     * @readonly
+     * @property PROVIDER_NAME
+     * @type {String}
+     */
+    var PROVIDER_NAME = 'google_recaptcha';
+
+    /**
+     * Called when the application is being installed for the first time.
+     *
+     * @param cb A callback that must be called upon completion.  cb(err, result).
+     * The result is ignored
+     */
+    ReCaptcha.onInstall = function(cb) {
+        cb(null, true);
+    };
+
+    /**
+     * Called when the application is uninstalling this plugin.  The plugin should
+     * make every effort to clean up any plugin-specific DB items or any in function
+     * overrides it makes.
+     *
+     * @param cb A callback that must be called upon completion.  cb(err, result).
+     * The result is ignored
+     */
+    ReCaptcha.onUninstall = function(cb) {
+        cb(null, true);
+    };
+
+    /**
+     * Called when the application is starting up. The function is also called at
+     * the end of a successful install. It is guaranteed that all core PB services
+     * will be available including access to the core DB.
+     *
+     * @param cb A callback that must be called upon completion.  cb(err, result).
+     * The result is ignored
+     */
+    ReCaptcha.onStartup = function(cb) {
+        var pluginService = new pb.PluginService();
+        pb.TemplateService.registerGlobal('recaptcha_div',function(flag,callback){
+            pluginService.getSetting('google_recaptcha_site_key', 'recaptcha-pencilblue', function(err,setting){
+                if(util.isError(err)){
+                    callback(err,null);
+                }
+                var divText = '<div class="g-recaptcha" data-sitekey="' + setting +  '"></div>';
+                callback(null, new pb.TemplateValue(divText,false));
+            });
+        });
+        var script = "<script src='https://www.google.com/recaptcha/api.js'></script>";
+        pb.TemplateService.registerGlobal('recaptcha_script',new pb.TemplateValue(script,false));
+        cb(null, true);
+    };
+
+    /**
+     * Called when the application is gracefully shutting down.  No guarantees are
+     * provided for how much time will be provided the plugin to shut down.
+     *
+     * @param cb A callback that must be called upon completion.  cb(err, result).
+     * The result is ignored
+     */
+    ReCaptcha.onShutdown = function(cb) {
+        cb(null, true);
+    };
+
+    //exports
+    return ReCaptcha;
+};
---
+++ services/recaptchaservice.js
@@ -0,0 +1,111 @@
+/**
+ * Google ReCaptcha for PencilBlue ReCaptcha Service
+ * @copyright Thomas H Case 2015 
+ * @author Thomas H Case
+ * @license This code is licensed under MIT license (see the LICENSE file for details)
+ */
+//dependencies
+var https = require('https');
+
+module.exports = function(pb) {
+  //Dependencies
+  var util          = pb.util
+  var PluginService = pb.PluginService;
+  var pluginService = new PluginService();
+
+  // Initialize the service object
+  function ReCaptchaService() {};
+
+  /**
+	* The name the service
+	* @private
+	* @static
+	* @readonly
+	* @property SERVICE_NAME
+	* @type {String}
+	*/
+  var SERVICE_NAME = 'ReCaptchaService'; 
+	
+  // This function will be called when PencilBlue loads the service
+  ReCaptchaService.init = function(cb) {
+    pb.log.debug(SERVICE_NAME + ": Initialized");
+    cb(null, true);
+  };
+
+  /**
+	* A service interface function designed to allow developers to name the handle 
+	* to the service object what ever they desire. The function must return a 
+	* valid string and must not conflict with the names of other services for the 
+	* plugin that the service is associated with.
+	*
+	* @static
+	* @method getName
+	* @return {String} The service name
+	*/
+  ReCaptchaService.getName = function() {
+	return SERVICE_NAME;
+  };
+  
+  /**
+    * Plugin Name
+    * @private
+	* @static
+	* @readonly
+	* @property PLUGIN_NAME
+	* @type {String}
+    */
+  var PLUGIN_NAME = 'recaptcha-pencilblue';
+  
+  /**
+   * Private Key Setting Name
+   * @private
+   * @static
+   * @readonly
+   * @property RECAPTCHA_PRIVATE_KEY_SETTING_NAME
+   * @type {String}
+   */
+  var RECAPTCHA_PRIVATE_KEY_SETTING_NAME = 'google_recaptcha_secret_key';
+  
+  /**
+   * Validate passed key
+   * @param data Post Data
+   * @param {function}cb Callback in format of (err,result)
+   */
+  ReCaptchaService.validateResponse = function(data,cb) {
+    var key = data["g-recaptcha-response"];
+    if(key) {
+      pluginService.getSetting(RECAPTCHA_PRIVATE_KEY_SETTING_NAME,PLUGIN_NAME,function(err,privateKey){
+        if(util.isError(err)){
+          cb(err,false);
+        }
+        var url = "https://www.google.com/recaptcha/api/siteverify?secret=" + privateKey + "&response=" + key;
+        https.get(url,function(res){
+          var data = "";
+          res.on('data',function(chunk){
+            data += chunk.toString();
+          });
+          res.on('end',function(){
+            try {
+              var parsedData = JSON.parse(data);
+              if(parsedData.success){
+                cb(null,parsedData.success);
+              } else {
+                var msg = "The following errors occurred: ";
+                msg += parsedData.error-codes.join();
+                var err = new Error(msg);
+                cb(err,parsedData.success);
+              }
+            } catch(err) {
+              cb(err,false);
+            }
+          });
+        });
+      });
+    } else {
+      var err = new Error("ReCaptcha respose not present");
+      cb(err,null);
+    }
+  };
+  
+  return ReCaptchaService;
+};
